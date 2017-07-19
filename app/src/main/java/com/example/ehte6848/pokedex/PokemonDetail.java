79fb0875d5c312c3bc5ae27dea5987767cb3fc9a
EhteshamIzhar <ehte6848@gmail.com>
2017-07-19 18:11:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ehte6848/pokedex/PokemonDetail.java
@@ -0,0 +1,35 @@
+package com.example.ehte6848.pokedex;
+
+/**
+ * Created by ehte6848 on 19-07-2017.
+ */
+
+public class PokemonDetail
+{
+
+    //set methods
+    private String name, height, weight;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setHeight(String height) {
+        this.height = height;
+    }
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+
+    // get methods
+    public String getName() {
+        return name;
+    }
+    public String getHeight() {
+        return height;
+    }
+    public String getWeight() {
+        return weight;
+    }
+
+
+}
